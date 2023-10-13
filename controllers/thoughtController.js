168658998e6397ef54c9e9e54b7f2cdd6967c5fb
JGatz123 <132381091+JGatz123@users.noreply.github.com>
2023-10-13 14:35:45 -0400

    added crud methods to thought controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,113 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+  // get all thoughts
+  async getThoughts(req, res) {
+    try {
+      const ThoughtData = await Thought.find()
+
+
+      res.json(ThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+ 
+  async getSingleThought(req, res) {
+    try {
+      const ThoughtData = await Thought.findOne({ _id: req.params.thoughtId });
+console.log(ThoughtData)
+      res.json(ThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+ 
+  async createThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.create(req.body);
+
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: dbThoughtData._id } },
+        { new: true }
+      )
+
+      res.json({ message: 'Thought successfully created!' });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  async updateThought(req, res) {
+    const houghtData = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true });
+
+
+    res.json(thoughtData);
+
+    console.log(err);
+    res.status(500).json(err);
+  },
+  // delete thought
+  async deleteThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndRemove({ _id: req.params.thoughtId })
+
+    
+
+      // remove thought from a user's thoughts array
+      const dbUserData = User.findOneAndUpdate(
+        { thoughts: req.params.thoughtId },
+        { $pull: { thoughts: req.params.thoughtId } },
+        { new: true }
+      );
+
+
+      res.json({ message: 'Thought deleted.' });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // update thought by adding a reaction to reactions array
+  async addReaction(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+
+
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  //same as above, except removing a reaction from reaction array
+  async removeReaction(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+      );
+
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+};
+
+module.exports = thoughtController;
--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,6 @@ const userController = {
   async getUsers(req, res) {
     try {
       const dbUserData = await User.find()
-        .select('-__v')
 
       res.json(dbUserData);
     } catch (err) {
@@ -17,9 +16,7 @@ const userController = {
   async getSingleUser(req, res) {
     try {
       const dbUserData = await User.findOne({ _id: req.params.userId })
-        .select('-__v')
         .populate('friends')
-        .populate('thoughts');
 
       if (!dbUserData) {
         return res.status(404).json({ message: 'No user with this id!' });
